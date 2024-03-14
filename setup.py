ba3cd72cef31f6db76f1800157baffe9ca422e5b
Metamer <100166407+jenkinsm13@users.noreply.github.com>
2024-03-14 16:10:34 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='JentalkIDE',
-    version='0.1.4',
+    version='0.1.5',
     author='Mike Jenkins',
     author_email='mike@chrotonics.com',
     packages=find_packages(),
